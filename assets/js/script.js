5bb3a5f7ca600b4d6d7a0f2f29d69084c1409a73
malmstrm <malmstrm.stefan@gmail.com>
2025-02-27 15:02:02 +0100

    Små ändringar om CV knappen

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
         navList.classList.toggle("menu-active");
     });
 
-    // Lägg till en eventlyssnare på varje länk i nav-listan
     const navLinks = document.querySelectorAll(".nav-content ul li a");
     navLinks.forEach(link => {
         link.addEventListener("click", function () {
