55bb655ed9d8c7b0b7e1cb8044f4846f8811741c
malmstrm <malmstrm.stefan@gmail.com>
2025-02-27 13:33:26 +0100

    WeatherAPI CSS+ Icon+ JS+

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,5 +36,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
         }
     });
+
+    const iconCode = data.weather[0].icon; // Hämtar ikon-koden från API-svaret
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    const cityInput = document.getElementById("city-input");
+    const searchBtn = document.getElementById("search-btn");
+    const cityLabel = document.getElementById("city-label");
+    const tempLabel = document.getElementById("temp-label");
+
+    const API_KEY ="42e7ac043437beb71b6c3a3a0a5f2655"; // Ersätt med din faktiska API-nyckel
+
+    function fetchWeather(city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            if(data.cod === 200) {
+            // Uppdatera etiketterna med stadens namn och temperaturen (avrundad)
+            cityLabel.textContent = data.name;
+            tempLabel.textContent = `${Math.round(data.main.temp)}°C`;
+            } else {
+            cityLabel.textContent = "Stad ej hittad";
+            tempLabel.textContent = "";
+            }
+        })
+        .catch(error => {
+            console.error("Fel vid hämtning av väderdata:", error);
+            cityLabel.textContent = "Fel";
+            tempLabel.textContent = "";
+        });
+    }
+    fetchWeather("Stockholm");
+
+    searchBtn.addEventListener("click", function() {
+        const city = cityInput.value.trim();
+        if(city !== "") {
+        fetchWeather(city);
+        }
+    });
 });
 
